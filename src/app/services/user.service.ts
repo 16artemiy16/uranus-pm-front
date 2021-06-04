0571ed432b47b234628df5a1612a74e019ed541b
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-06-04 12:11:48 +0300

    Implement Sign In

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,8 +19,18 @@ export class UserService {
     return this.http.get<boolean>(url);
   }
 
-  signUp(data: { email: string, password: string }) {
+  signUp(data: { email: string, password: string }): Observable<boolean> {
     const url = `${this.URL}/users`;
-    return this.http.post(url, data);
+    return this.http.post<boolean>(url, data);
+  }
+
+  signIn(email: string, password: string): Observable<string> {
+    const url = `${this.URL}/users/auth`;
+    return this.http.post<string>(url, { email, password }, {responseType: 'text' as 'json'})
+      .pipe(
+        tap((token) => {
+          localStorage.setItem('token', token);
+        })
+      );
   }
 }
