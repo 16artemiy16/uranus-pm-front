41c00668f9b165c72799330a3296af15d6e43ae8
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-07-05 16:43:51 +0300

    Make TeamManagement component onPush and small refactoring

--- src/app/modules/auth-user/modules/pm/components/modals/team-management/team-management.component.ts
+++ src/app/modules/auth-user/modules/pm/components/modals/team-management/team-management.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { UserService } from '../../../../../../../services/user.service';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { debounceTime, startWith, switchMap, takeUntil, withLatestFrom } from 'rxjs/operators';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { UserI } from '../../../../../../../interfaces/user.interface';
 import { BoardsSandbox } from '../../../store/sandboxes/boards.sandbox';
 import { BoardUserI } from '../../../../../../../interfaces/board-user.interface';
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-team-management',
   templateUrl: './team-management.component.html',
-  styleUrls: ['./team-management.component.scss']
+  styleUrls: ['./team-management.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TeamManagementComponent implements OnDestroy {
   readonly userSearchControl: FormControl = this.fb.control('');
@@ -26,12 +26,19 @@ export class TeamManagementComponent implements OnDestroy {
     private readonly fb: FormBuilder,
     private readonly boardsSandbox: BoardsSandbox
   ) {
+    this._watchBoardMembers();
+    this._watchUserSearch();
+  }
+
+  private _watchBoardMembers(): void {
     this.boardsSandbox.boardMembers$
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe((members) => {
         this.usersSource.data = members;
       });
+  }
 
+  private _watchUserSearch(): void {
     this.userSearchControl.valueChanges
       .pipe(
         debounceTime(500),
@@ -43,7 +50,7 @@ export class TeamManagementComponent implements OnDestroy {
         startWith([]),
         takeUntil(this.unsubscribe$)
       ).subscribe((users) => {
-        this.inviteUsersList = users;
+      this.inviteUsersList = users;
     });
   }
 
