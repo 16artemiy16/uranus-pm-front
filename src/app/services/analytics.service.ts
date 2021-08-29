a1b44a2e00737bbf403c005a10493a784809ccd2
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-08-29 17:39:26 +0300

    Add debounce to tracking user visit board

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 type UserEventActionT = 'visit';
 type UserEventVisitData = {
@@ -14,6 +15,8 @@ type UserEventDataT = UserEventVisitData;
 })
 export class AnalyticsService {
   private readonly URL = 'http://localhost:3000/analytics';
+  private readonly VISIT_BOARD_TRACKING_DEBOUNCE_MS = 600000 // 10 min
+  private boardsLastsVisitsMs: Record<string, number> = {}
 
   constructor(private readonly http: HttpClient) { }
 
@@ -22,8 +25,17 @@ export class AnalyticsService {
     return this.http.post(url, { action, data })
   }
 
-  traceUserVisitBoard(board: string): Observable<any> {
+  traceUserVisitBoard(board: string): Observable<any | null> {
+    const lastVisitMs = this.boardsLastsVisitsMs[board];
+    const currentVisitMs = new Date().getTime();
+
+    if (lastVisitMs && (currentVisitMs - lastVisitMs < this.VISIT_BOARD_TRACKING_DEBOUNCE_MS)) {
+      return of(null);
+    }
+
     const data: UserEventVisitData = { targetType: 'board', targetId: board };
-    return this.traceUserEvent('visit', data);
+    return this.traceUserEvent('visit', data).pipe(
+      tap(() => this.boardsLastsVisitsMs[board] = currentVisitMs)
+    );
   }
 }
