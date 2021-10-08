87c48a641f91fd6042575f0c0d67f72109a88759
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-10-08 10:36:59 +0300

    Implement profile dump layout

--- src/app/modules/auth-user/modules/profile/pages/profile/profile.component.ts
+++ src/app/modules/auth-user/modules/profile/pages/profile/profile.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { UserService } from '../../../../../../services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent {
+  readonly userImgUrl = 'url(https://s3.amazonaws.com/assets.saam.media/files/styles/x_large/s3/files/images/1966/SAAM-1966.48.26_1.jpg?itok=_g0Ompts)';
+  readonly user = this.userService.currentUser;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(
+    private readonly userService: UserService
+  ) {}
 
 }
--- src/app/modules/auth-user/modules/profile/profile.module.ts
+++ src/app/modules/auth-user/modules/profile/profile.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -13,7 +15,9 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     RouterModule.forChild([
       { path: '', component: ProfileComponent }
-    ])
+    ]),
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class ProfileModule { }
